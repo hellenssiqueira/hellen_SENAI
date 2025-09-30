4de1242286b632da1cd4f5845a4a110defe8051a
Hellen Siqueira <hellen.ssiqueira@aluno.edu.es.gov.br>
2025-09-30 16:38:24 -0300

    exercícios.02

---
+++ exercício.02/exercício.02/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("digite um número:");
+int numero = int.Parse(Console.ReadLine());
+
+int antecessor = numero - 1;
+int sucessor = numero + 1;
+Console.WriteLine("O antecessor é " +antecessor);
+Console.WriteLine("O sucessor é " + sucessor);
+
