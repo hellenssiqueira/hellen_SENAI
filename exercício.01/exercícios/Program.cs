a4c2f2d44f0152f8304d708b2e91ed71e85f6a68
Hellen Siqueira <hellen.ssiqueira@aluno.edu.es.gov.br>
2025-09-30 16:37:07 -0300

    exercícios.01

---
+++ exercício.01/exercícios/Program.cs
@@ -0,0 +1,14 @@
+﻿int A = 10;
+int B = 20;
+
+Console.WriteLine("valores iniciais:");
+Console.WriteLine("A = " + A);
+Console.WriteLine("B = " + B);
+
+A = A + B;
+B = A - B;
+A = A - B;
+
+Console.WriteLine("\nvalores após a troca:");
+Console.WriteLine("A = " + A);
+Console.WriteLine("B = " + B);
