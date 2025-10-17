2cfc4a33ea978c82b3a10345a1497c889ff90af3
Hellen Siqueira <hellen.ssiqueira@aluno.edu.es.gov.br>
2025-10-17 17:03:17 -0300

    Add files via upload

---
+++ questão.09 - LOG/questão.09/Program.cs
@@ -0,0 +1,18 @@
+﻿string[] nomes = { "Ana", "Bruno", "Carlos", "Diana", "Eduardo" };
+Console.Write("Digite um nome para buscar: ");
+string nomeBusca = Console.ReadLine();
+bool encontrado = false; 
+                      
+foreach (string nome in nomes)
+{
+    if (nome.Equals(nomeBusca, StringComparison.OrdinalIgnoreCase))
+    {
+        encontrado = true;
+        break; 
+    }
+}
+if (encontrado)
+    Console.WriteLine($"O nome {nomeBusca} foi encontrado no vetor.");
+else
+    Console.WriteLine($"O nome {nomeBusca} não está no vetor.");
+
