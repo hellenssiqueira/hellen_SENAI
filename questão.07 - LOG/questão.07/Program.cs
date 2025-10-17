c1fc8efd7d11ade333c9419dc0a4d5456d2ebdf6
Hellen Siqueira <hellen.ssiqueira@aluno.edu.es.gov.br>
2025-10-17 16:41:48 -0300

    Add files via upload

---
+++ questão.07 - LOG/questão.07/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Digite o número inicial(start): ");
+int start = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o número final (end): ");
+int end = int.Parse(Console.ReadLine());
+
+if (start > end)
+{
+    int temp = start;
+    start = end;
+    end = temp;
+}
+
+for (int n = start; n < end; n++)
+{
+    Console.WriteLine($"Tabuada do {n}:");
+
+    for (int i = 1; i < 10; i++)
+    {
+        int resultado = n * i;
+        Console.WriteLine($"{n} x {i} = {resultado}");
+    }
+    Console.WriteLine();
+}
+
