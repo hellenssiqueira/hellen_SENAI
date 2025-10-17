f1f037486518a943566800993f0e0d0d6ce955a3
Hellen Siqueira <hellen.ssiqueira@aluno.edu.es.gov.br>
2025-10-17 16:53:08 -0300

    Add files via upload

---
+++ questão.08 - LOG/questão.08/Program.cs
@@ -0,0 +1,34 @@
+﻿int voto;
+int candidatoA = 0, candidatoB = 0, candidatoC = 0, nulos = 0;
+
+while (true)
+{
+    Console.WriteLine("\nDigite seu voto:");
+    Console.WriteLine("1 - Candidato A");
+    Console.WriteLine("2 - Candidato B");
+    Console.WriteLine("3 - Candidato C");
+    Console.WriteLine("4 - Voto Nulo");
+    Console.WriteLine("0 - Encerrar votação");
+    Console.Write("Opção: ");
+    voto = int.Parse(Console.ReadLine());
+    if (voto == 0) break; 
+                        
+    switch (voto)
+    {
+        case 1:
+            candidatoA++;
+            break;
+        case 2:
+            candidatoB++;
+            break;
+        case 3:
+            candidatoC++;
+            break;
+        case 4:
+            nulos++;
+            break;
+        default:
+            Console.WriteLine("Voto inválido! Tente novamente.");
+            break;
+    }
+}
